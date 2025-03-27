9fab039430649f44d0aec3b0258d2397dff8b7a7
Matheus Coelho <matheuscoelho060@gmail.com>
2025-03-26 22:03:06 -0300

    Fix BackEnd

--- src/services/client.ts
+++ src/services/client.ts
@@ -6,7 +6,7 @@ export const client = createClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: 'production',
     useCdn: false,
-    token: process.env.SANITY_PROJECT_TOKEN,
+    token: process.env.NEXT_PUBLIC_SANITY_PROJECT_TOKEN,
     apiVersion: '2024-03-01'
 });
 
